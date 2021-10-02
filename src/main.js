292737a495bd98197b5cecdfdd34b4c05e3419b0
Muntari29 <muntrock@gmail.com>
2021-10-02 18:48:42 +0900

    Add: App.vue 파일 추가 및 Vue, webpack 관련 패키지 추가 설치

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
-console.log('bundle Test')
+import { createApp } from "vue";
+import App from "./App.vue"
+
+createApp(App).mount('#app')
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,29 @@
 const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/main.js',
     output: {
-        path: path.resolve(__dirname, 'dist')
-    }
-}
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: 'vue-loader',
+        },
+        {
+          test: /\.s?css$/,
+          use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new HtmlPlugin({
+        template: './src/index.html', //웹팩이 해석해야하는 html 파일이 어디있는지 명시함
+      })
+    ]
+  }
